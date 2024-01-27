3b618e24a5a24819c9ea15a55fe169abaf713001
chandud-devops <sm2chandu@gmail.com>
2024-01-27 06:37:23 +0000

    created 01-string-substring.py,02-float.py

---
+++ Day-02/examples/01-string-substring.py
@@ -0,0 +1,6 @@
+# # SUBSTRING
+
+text = "Python are awesome"
+substring = "are"
+if substring in text:
+ print(substring, "found in the text")
---
+++ Day-02/examples/02-float.py
@@ -0,0 +1,26 @@
+# Flot variables
+num1 = 5.123456
+num2 = 2.5
+# Basic Arithmetic
+
+result1 = num1 + num2
+print("Addition:", result1)
+
+
+result2 = num1 - num2
+print("Substration:", result2)
+
+result3 = num1 * num2
+print("Multiplication:", result3)
+
+result4 = num1 / num2
+print("Division:", result4)
+
+# Rounding
+result5 = round(result1, 2)
+result6 = round(result2, 3)
+# Rounds to 2 decimal places
+print("Rounded:", result5)
+print("Round of sub:", result6)
+
+
